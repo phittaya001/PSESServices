941d9256ca200283d16c080e8bc713a81d647a43
phittaya001 <phittayajenkhetkarn@gmail.com>
2017-02-24 13:54:58 +0700

    resolt period

--- PESproj/Controllers/EvaController.cs
+++ PESproj/Controllers/EvaController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using PesWeb.Service.Modules;
 
 namespace PESproj.Controllers
 {
@@ -15,11 +16,22 @@ namespace PESproj.Controllers
     {
         [Route("Period")]
         [HttpGet]
-        public List<Period> GetPeriod()
+        public List<PeriodData> GetPeriod()
         {
             var header = ServiceContainer.GetService<PesWeb.Service.Modules.EvaManage>();
             List<Period> GetPeriod = header.GetAllPeriod().ToList();
-            return GetPeriod;
+            List<PeriodData> pd = new List<PeriodData>();
+            foreach(Period p in GetPeriod)
+            {
+                PeriodData newp = new PeriodData();
+                newp.Code = p.Code;
+                newp.Period_Id = p.Period_Id;
+                newp.StartDate = p.StartDate.ToString().Substring(0,10);
+                newp.FinishDate = p.FinishDate.ToString().Substring(0, 10);
+                pd.Add(newp);
+            }
+            return pd;
         }
+
     }
 }
---
+++ PesWeb/Models/PesWeb.Service/Modules/PeriodData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesWeb.Service.Modules
+{
+    public class PeriodData
+    {
+        public int Period_Id { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string FinishDate { get; set; }
+
+        public string Code { get; set; }
+
+        public Nullable<int> Status { get; set; }
+    }
+}
