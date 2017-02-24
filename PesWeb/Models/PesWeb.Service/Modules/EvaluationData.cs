4a6bd502efca2904559cb22b54e7d4c388095a33
phittaya001 <phittayajenkhetkarn@gmail.com>
2017-02-24 17:08:34 +0700

    3function

---
+++ PesWeb/Models/PesWeb.Service/Modules/EvaluationData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesWeb.Service.Modules
+{
+    class EvaluationData
+    {
+        public int Eva_ID { get; set; }
+
+        public string EmployeeNO { get; set; }
+
+        public string EvaluatorNO { get; set; }
+
+        public Nullable<System.DateTime> Date { get; set; }
+
+        public Nullable<int> Job_ID { get; set; }
+
+        public string ProjectNO { get; set; }
+
+        public string name { get; set; }
+    }
+}
